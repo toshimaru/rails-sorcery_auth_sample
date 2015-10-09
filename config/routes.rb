1318cda9d9ad248f91d786dd84127ff1ba6b3339
toshi <me@toshimaru.net>
2015-10-09 14:45:19 +0900

    User create / User session create

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
+  root :to => 'users#index'
 
-  get 'user_sessions/create'
-
-  get 'user_sessions/destroy'
-
+  resources :user_sessions
   resources :users
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
